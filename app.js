4ae50004a23e821a12eaecad48463658ca5ed93f
Husain Zaidi <husainhz7@gmail.com>
2018-01-30 16:31:33 +0530

    added post request handling v1

--- app.js
+++ app.js
@@ -1,9 +1,21 @@
 express = require('express')
+bodyParser = require('body-parser')
 var app = express()
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+text = "Hello World!"
+
+app.post('/', function(req,res){
+	text = req.body.name;
+	res.redirect('/')
+})
 
 app.get('/',function(req,res){
-	res.send("Hello World!")
+	res.send(text)
 })
+
+
 
 app.listen(process.env.PORT || 3000, function(){
 	console.log('Server Started')
